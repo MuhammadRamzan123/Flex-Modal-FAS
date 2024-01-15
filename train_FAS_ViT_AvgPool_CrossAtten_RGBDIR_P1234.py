f816e5971b9d16e0e9596b4d5e9e9642e2a66dd6
MuhammadRamzan123 <78503112+MuhammadRamzan123@users.noreply.github.com>
2024-01-15 06:53:02 -0800

    Update train_FAS_ViT_AvgPool_CrossAtten_RGBDIR_P1234.py

--- train_FAS_ViT_AvgPool_CrossAtten_RGBDIR_P1234.py
+++ train_FAS_ViT_AvgPool_CrossAtten_RGBDIR_P1234.py
@@ -32,8 +32,8 @@ from utils_FAS_MultiModal2 import AvgrageMeter, performances_FAS_MultiModal
 ##########    Dataset root    ##########
 
 # root_dir    CASIA_SURF_CeFA; WMCA
-casia_root_FAS_dir = '/kaggle/input/my-modal-data/'
-wmca_root_FAS_dir = '/kaggle/input/wmca-evaluation/'
+casia_root_FAS_dir = '/kaggle/input/my-modal-data/Model_Data/'
+wmca_root_FAS_dir = '/kaggle/input/wmca-evaluation/WMCA/'
 # train_list     CASIA_SURF_CeFA
 train_CASIA_SURF_CeFA_list = 'FlexModal_Protocols/CASIA-SURF_CeFA_train.txt'
 
