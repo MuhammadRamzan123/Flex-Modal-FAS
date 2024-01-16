5470666121cd5c0300d01cd2710f88295d56808e
MuhammadRamzan123 <78503112+MuhammadRamzan123@users.noreply.github.com>
2024-01-16 09:10:29 -0800

    Update Load_FAS_MultiModal_DropModal_test.py

--- Load_FAS_MultiModal_DropModal_test.py
+++ Load_FAS_MultiModal_DropModal_test.py
@@ -145,11 +145,10 @@ class Spoofing_valtest(Dataset):
         
         return image_x, binary_mask
      except Exception as e:
-        print(f"Error in get_single_image_x_RGB: {e}")
-        # Handle the error appropriately, you may choose to return a default image or raise an exception
-	image_x_aug = np.zeros((224, 224, 3))
-	binary_mask = np.zeros((28, 28))
-	return image_x_aug, binary_mask
+	     print(f"Error in get_single_image_x_RGB: {e}")
+	     image_x_aug = np.zeros((224, 224, 3))
+	     binary_mask = np.zeros((28, 28))
+	     return image_x_aug, binary_mask
 	     
     def get_single_image_x(self, image_path):
         
